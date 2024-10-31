188222e69dc316628139e1a55497204bba991053
Maxim Buryakovsky <mbur1717@gmail.com>
2024-10-31 10:25:21 +0300

    bugs fixed

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.PostsConfig',
+    'users.apps.UsersConfig',
 ]
 
 AUTH_USER_MODEL = 'users.User'
