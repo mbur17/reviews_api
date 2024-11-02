4c84ee0ce5e1af1dd0ccbc3dc7cdcbf3a3aa5be3
Maxim Buryakovsky <mbur1717@gmail.com>
2024-11-02 13:48:06 +0300

    add users views

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,10 +1,14 @@
 from django.contrib.auth import get_user_model
-from rest_framework import status
+from rest_framework import filters, viewsets, status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import AccessToken
 
-from .serializers import SignupSerializer, TokenSerializer
+from .permissions import IsAdminOrReadOnly
+from .serializers import (
+    SignupSerializer, TokenSerializer, UserSerializer, MeSerializer
+)
 
 User = get_user_model()
 
@@ -30,4 +34,28 @@ class TokenView(APIView):
             token = AccessToken.for_user(user)
             return Response({'token': str(token)}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
- 
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('username',)
+
+
+class UserMeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = MeSerializer(request.user)
+        return Response(serializer.data)
+
+    def patch(self, request):
+        serializer = MeSerializer(
+            request.user, data=request.data, partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
