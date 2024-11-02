b3dc3300f260dda9e0470286c85bdd0745096eb8
Maksim Polushin <btnchsky.sad@mail.ru>
2024-11-03 01:43:18 +0300

    rating field

--- api_yamdb/titles/serializers.py
+++ api_yamdb/titles/serializers.py
@@ -1,6 +1,10 @@
-from rest_framework.serializers import ModelSerializer, SlugRelatedField
+from rest_framework.serializers import (
+    ModelSerializer, SlugRelatedField, SerializerMethodField
+)
+from django.db.models import Sum
 
 from .models import Genre, Category, Title
+from reviews.models import Review
 
 
 class GenreSerializer(ModelSerializer):
@@ -28,6 +32,7 @@ class TitleSerializer(ModelSerializer):
         queryset=Category.objects.all(),
         slug_field='slug'
     )
+    rating = SerializerMethodField()
 
     class Meta:
         model = Title
@@ -36,6 +41,18 @@ class TitleSerializer(ModelSerializer):
             'name',
             'year',
             'description',
+            'rating',
             'genre',
             'category',
         )
+
+    def get_rating(self, obj):
+        """Вычисляет значение поля rating."""
+        queryset = Review.objects.filter(title=obj.id)
+        if not queryset:
+            return 0
+        summary = queryset.aggregate(
+            total_score=Sum('score')
+        )['total_score'] or 0
+        reviews_count = queryset.count()
+        return summary / reviews_count
