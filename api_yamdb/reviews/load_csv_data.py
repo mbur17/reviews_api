5e3a67f7279989f2c8953aaa95bc734b8548ba83
Maksim Polushin <btnchsky.sad@mail.ru>
2024-11-05 14:51:13 +0300

    load_csv_data script v1

--- api_yamdb/reviews/load_csv_data.py
+++ api_yamdb/reviews/load_csv_data.py
@@ -37,16 +37,39 @@ FIELDS = {
 }
 
 
+def change_foreign_values(data_csv):
+    """Изменяет значения."""
+    data_csv_copy = data_csv.copy()
+    for field_key, field_value in data_csv.items():
+        if field_key in FIELDS.keys():
+            field_key0 = FIELDS[field_key][0]
+            data_csv_copy[field_key0] = FIELDS[field_key][1].objects.get(
+                pk=field_value)
+    return data_csv_copy
+
+
+def load_csv(file, class_name):
+    data = open_csv(file)
+    rows = data[1:]
+    for row in rows:
+        data = dict(zip(data[0], row))
+        data = change_foreign_values(data)
+        try:
+            table = class_name(**data)
+            table.save()
+        except Exception as ex:
+            print(
+                f'возникла ошибка {ex} при загрузке '
+                f'таблицы {class_name.__qualname__}')
+            break
+    print(f'таблица {class_name.__qualname__}')
+
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for key, value in FILES_CLASSES.items():
             print(f'загрузка таблицы {value.__qualname__}')
-            path = os.path.join(CSV_FILES_DIR, f'{file_name}.csv')
-            try:
-                with open(path, encoding='utf-8') as file:
-                    return list(csv.reader(file))
-            except Exception as ex:
-                print(ex)
+            load_csv(key, value)
 
 
 def open_csv(file_name):
